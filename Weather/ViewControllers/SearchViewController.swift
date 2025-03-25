331d25e9a0faee7e19b64716392cd5f963b27f11
Darina Samokhina <mokhinsam@gmail.com>
2025-03-25 13:02:52 +0300

    Added connection between SearchVC and screens outlets. Added screen SearchVC in Storyboard protocols UITableViewDataSource, UITableViewDelegate.

--- Weather/ViewControllers/SearchViewController.swift
+++ Weather/ViewControllers/SearchViewController.swift
@@ -9,23 +9,18 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func downButtonDidPressed() {
     }
-    */
-
+    
 }
 
 //MARK: - UITableViewDataSource
